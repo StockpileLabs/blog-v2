1191cb5bc111d73dac0df18410182d19e5c38d12
adlonymous <adlonymous@gmail.com>
2024-01-26 21:51:34 -0500

    Going good so far

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const SITE_TITLE = "Stockpile Blog";
+export const SITE_DESCRIPTION =
+  "Read updates, project highlights, guides, case studies, round reviews, and more from the Stockpile Team";
+
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      spblack: "#000B3F",
+      spblue1: "#00DCF4",
+      spblue2: "#008CFF",
+      white: "#FFFFFF",
+      black: "#000000",
+      gray: colors.gray,
+      "accent-1": "#FAFAFA",
+      "accent-2": "#EAEAEA",
+      "accent-7": "#333",
+      success: "#0070f3",
+      cyan: "#79FFE1",
+      "blue-500": "#2276FC",
+      "yellow-100": "#fef7da",
+    },
+    fontFamily: {
+      poppins: ['"Poppins"', "sans-serif"],
+    },
+  },
+  plugins: [],
+};
