acc8027437a6489914c3ff2d2cf30e803bff2bdb
adlonymous <adlonymous@gmail.com>
2024-01-28 13:09:52 -0500

    Added dynamic homepage data

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const SITE_TITLE = "Stockpile Blog";
 export const SITE_DESCRIPTION =
   "Read updates, project highlights, guides, case studies, round reviews, and more from the Stockpile Team";
 
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
 export const ARTICLES_PER_PAGE = 6;
